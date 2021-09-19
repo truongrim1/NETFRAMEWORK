0477bf7d901668cf0e124ddebb69b82b33b148fb
truong cong vi <truongrim1@gmail.com>
2021-09-19 19:13:29 +0700

    first commit

---
+++ BasicWinform/BasicWinform/User.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace BasicWinform
+{
+    partial class User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(User));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông tin cá nhân";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(28, 182);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 266);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lí Lịch";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Họ tên:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ngày Sinh:";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Giới tính:";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Quê Quán:";
+            this.label5.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(70, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(443, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(70, 79);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(443, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.TabStop = false;
+            this.dateTimePicker1.Value = new System.DateTime(2000, 11, 11, 0, 0, 0, 0);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(70, 122);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nam";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(139, 122);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Nữ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(208, 120);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "Khác";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(70, 153);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(443, 80);
+            this.textBox2.TabIndex = 1;
+            // 
+            // User
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 502);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "User";
+            this.Text = "Thông tin cá nhân";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ BasicWinform/BasicWinform/frmMain.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicWinform
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var soThuNhat = txtSoThuNhat.Text;
+                double temp = 0, nSoThuNhat = 0, nSoThuHai = 0;
+                if (double.TryParse(soThuNhat, out temp))
+                    nSoThuNhat = Convert.ToDouble(soThuNhat);
+                else
+                {
+                    MessageBox.Show
+                    (
+                        $"Vui lòng nhập số thứ nhất",
+                        "Thông báo lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+                var soThuHai = txtSoThuHai.Text;
+                if (double.TryParse(soThuHai, out temp))
+                    nSoThuHai = Convert.ToDouble(soThuHai);
+                else
+                {
+                    MessageBox.Show(
+                         $"Vui lòng nhập số thứ hai",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+                    return;
+                }
+
+                var KetQua = nSoThuNhat + nSoThuHai;
+                lblKetQua.Text = KetQua.ToString("N0");
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi định dạng. vui lòng nhập số: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi tràn số. Vui lòng nhập ngắn lại: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                         $"lỗi. Chi tiết lỗi: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var soThuNhat = txtSoThuNhat.Text;
+                double temp = 0, nSoThuNhat = 0, nSoThuHai = 0;
+                if (double.TryParse(soThuNhat, out temp))
+                    nSoThuNhat = Convert.ToDouble(soThuNhat);
+                else
+                {
+                    MessageBox.Show
+                    (
+                        $"Vui lòng nhập số thứ nhất",
+                        "Thông báo lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+                var soThuHai = txtSoThuHai.Text;
+                if (double.TryParse(soThuHai, out temp))
+                    nSoThuHai = Convert.ToDouble(soThuHai);
+                else
+                {
+                    MessageBox.Show(
+                         $"Vui lòng nhập số thứ hai",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+                    return;
+                }
+
+                var KetQua = nSoThuNhat - nSoThuHai;
+                lblKetQua.Text = KetQua.ToString("N0");
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi định dạng. vui lòng nhập số: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi tràn số. Vui lòng nhập ngắn lại: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                         $"lỗi. Chi tiết lỗi: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var soThuNhat = txtSoThuNhat.Text;
+                double temp = 0, nSoThuNhat = 0, nSoThuHai = 0;
+                if (double.TryParse(soThuNhat, out temp))
+                    nSoThuNhat = Convert.ToDouble(soThuNhat);
+                else
+                {
+                    MessageBox.Show
+                    (
+                        $"Vui lòng nhập số thứ nhất",
+                        "Thông báo lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+                var soThuHai = txtSoThuHai.Text;
+                if (double.TryParse(soThuHai, out temp))
+                    nSoThuHai = Convert.ToDouble(soThuHai);
+                else
+                {
+                    MessageBox.Show(
+                         $"Vui lòng nhập số thứ hai",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+                    return;
+                }
+
+                var KetQua = nSoThuNhat * nSoThuHai;
+                lblKetQua.Text = KetQua.ToString("N0");
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi định dạng. vui lòng nhập số: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi tràn số. Vui lòng nhập ngắn lại: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                         $"lỗi. Chi tiết lỗi: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var soThuNhat = txtSoThuNhat.Text;
+                double temp = 0, nSoThuNhat = 0, nSoThuHai = 0;
+                if (double.TryParse(soThuNhat, out temp))
+                    nSoThuNhat = Convert.ToDouble(soThuNhat);
+                else
+                {
+                    MessageBox.Show
+                    (
+                        $"Vui lòng nhập số thứ nhất",
+                        "Thông báo lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+                var soThuHai = txtSoThuHai.Text;
+                if (double.TryParse(soThuHai, out temp))
+                    nSoThuHai = Convert.ToDouble(soThuHai);
+                else
+                {
+                    MessageBox.Show(
+                         $"Vui lòng nhập số thứ hai",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+                    return;
+                }
+
+                var KetQua = nSoThuNhat / nSoThuHai;
+                lblKetQua.Text = KetQua.ToString("N0");
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi định dạng. vui lòng nhập số: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(
+                         $"lỗi tràn số. Vui lòng nhập ngắn lại: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                         $"lỗi. Chi tiết lỗi: {ex.Message}",
+                         "Thông báo lỗi",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+            }
+        }
+    }
+}
